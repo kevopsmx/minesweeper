9e9346b2707e9b654ea821e0e16c53af4e2ac8b2
Martin Rodriguez <mrodriguez@efxfs.com>
2021-09-30 09:33:38 -0500

    Update Game.java
    Init the mines
    Display the board

--- src/main/java/com/deviget/minesweeper/model/Game.java
+++ src/main/java/com/deviget/minesweeper/model/Game.java
@@ -52,6 +52,16 @@ public class Game {
 		for(int i=0;i<cols*rows;i++) {
 			board[i] = "" + COVER;
 		}
+
+		//put the mines on random positions
+		Random random = new Random();
+		for(int i=0;i<mines;) {
+			int current = random.nextInt(total-1);
+			if(board[current].indexOf(MINE) == -1) {
+				board[current] = board[current] + MINE;
+				i++;
+			}
+		}
 	}
 	
 	/**
@@ -72,11 +82,25 @@ public class Game {
 	    return generatedString;
 	}
 
+	/**
+	 * Display the board in human readable form, comma sepparated rows and columns
+	*/
+	public String displayBoard() {
+		StringBuilder bld = new StringBuilder();
+		for(int y=0;y<rows;y++) {
+			for(int x=0;x<cols;x++) {
+				int pos = y * rows + x;
+				bld.append(board[pos]);
+				if(x+1<cols)
+					bld.append(",");
+			}
+			bld.append("\n");
+		}
+		return bld.toString();
+	}
+
 	public static void main(String args[]) {
 		Game game = new Game(5,5,1,"martin");
-		System.out.println(game.id);
-		for(String cell:game.board) {
-			System.out.println(cell);
-		}
+		System.out.println(game.displayBoard());
 	}
 }
