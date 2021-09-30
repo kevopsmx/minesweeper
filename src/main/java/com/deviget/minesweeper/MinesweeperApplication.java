caf424e05967332773990928aa7685187db41fdb
Martin Rodriguez <mrodriguez@efxfs.com>
2021-09-30 09:15:22 -0500

    Initial Project setup
    SpringBoot base project

---
+++ src/main/java/com/deviget/minesweeper/MinesweeperApplication.java
@@ -0,0 +1,12 @@
+package com.deviget.minesweeper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperApplication.class, args);
+	}
+
+}
